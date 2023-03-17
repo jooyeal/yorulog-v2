8c95999edb60307db7b3e885103d6f4acea48a94
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-03-17 23:52:11 +0900

    ADD: markdown library

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -1,8 +1,23 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import "@uiw/react-md-editor/markdown-editor.css";
+import "@uiw/react-markdown-preview/markdown.css";
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import Layout from "@/components/common/Layout";
+
+const MDEditor = dynamic(() => import("@uiw/react-md-editor"), { ssr: false });
+const MarkdownPreview = dynamic(() => import("@uiw/react-markdown-preview"), {
+  ssr: false,
+});
 
 const PostCreate = () => {
-  return <Box>PostCreate</Box>;
+  const [value, setValue] = useState<string | undefined>("**Hello world!!!**");
+
+  return (
+    <Layout>
+      <MDEditor value={value} onChange={(e) => setValue(e)} highlightEnable />
+      <MarkdownPreview source={value} />
+    </Layout>
+  );
 };
 
 export default PostCreate;
