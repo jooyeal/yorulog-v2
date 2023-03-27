d81ac2ce2ca3e96fb521b10f12adb4361ec40d17
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-03-27 11:50:51 +0900

    FIX: modify trpc setting

--- src/utils/trpc.ts
+++ src/utils/trpc.ts
@@ -1,6 +1,7 @@
 import { httpBatchLink } from "@trpc/client";
 import { createTRPCNext } from "@trpc/next";
 import type { AppRouter } from "../server/routers/_app";
+import superjson from "superjson";
 
 function getBaseUrl() {
   if (typeof window !== "undefined")
@@ -21,21 +22,59 @@ function getBaseUrl() {
 
 export const trpc = createTRPCNext<AppRouter>({
   config({ ctx }) {
+    if (typeof window !== "undefined") {
+      // during client requests
+      return {
+        transformer: superjson,
+        links: [
+          httpBatchLink({
+            url: "/api/trpc",
+          }),
+        ],
+      };
+    }
     return {
+      transformer: superjson,
       links: [
         httpBatchLink({
-          /**
-           * If you want to use SSR, you need to use the server's full URL
-           * @link https://trpc.io/docs/ssr
-           **/
+          // The server needs to know your app's full url
           url: `${getBaseUrl()}/api/trpc`,
+          /**
+           * Set custom request headers on every request from tRPC
+           * @link https://trpc.io/docs/v10/header
+           */
+          headers() {
+            if (ctx?.req) {
+              // To use SSR properly, you need to forward the client's headers to the server
+              // This is so you can pass through things like cookies when we're server-side rendering
+              // If you're using Node 18, omit the "connection" header
+              const { connection: _connection, ...headers } = ctx.req.headers;
+              return {
+                ...headers,
+                // Optional: inform server that it's an SSR request
+                "x-ssr": "1",
+              };
+            }
+            return {};
+          },
         }),
       ],
-      /**
-       * @link https://tanstack.com/query/v4/docs/reference/QueryClient
-       **/
-      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
+    // return {
+    //   links: [
+    //     httpBatchLink({
+    //       /**
+    //        * If you want to use SSR, you need to use the server's full URL
+    //        * @link https://trpc.io/docs/ssr
+    //        **/
+    //       url: `${getBaseUrl()}/api/trpc`,
+    //     }),
+    //   ],
+    //   /**
+    //    * @link https://tanstack.com/query/v4/docs/reference/QueryClient
+    //    **/
+    //   // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
+    // };
   },
   /**
    * @link https://trpc.io/docs/ssr
