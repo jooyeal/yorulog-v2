1a20f54b9064f56109d997c3a6bbb944d2ff4398
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-04-23 22:21:29 +0900

    ADD: Progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
+import NextNProgress from "nextjs-progressbar";
 import { trpc } from "../utils/trpc";
 
 export default trpc.withTRPC(function App({
@@ -10,6 +11,13 @@ export default trpc.withTRPC(function App({
 }: AppProps) {
   return (
     <SessionProvider session={session}>
+      <NextNProgress
+        color="#81E6D9"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
