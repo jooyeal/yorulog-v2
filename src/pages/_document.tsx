e8abc7aaf1c1310884d72753cddeef315f055d84
jooyeal <34053954+jooyeal@users.noreply.github.com>
2023-04-24 17:51:04 +0900

    FIX: for verify google console seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,10 @@ import { ColorModeScript } from "@chakra-ui/react";
 export default function Document() {
   return (
     <Html lang="en">
+      <meta
+        name="google-site-verification"
+        content="m1SemBs2ZdWy3yinTbP89KzrWF00CoEYFClegylQqAE"
+      />
       <Head />
       <body>
         <ColorModeScript initialColorMode="dark" />
